6dffc3fb1477c854635a17300b9de257f04c44ab
Your Name <iuliailies>
2021-04-16 00:14:06 +0300

    color changes

--- GUI_improved.py
+++ GUI_improved.py
@@ -553,14 +553,6 @@ class GUI:
         self.convert_board_to_interface()
         self.create_status_bars()
 
-    # def save_changes(self, root):
-    #     # self.set_board_color(color)
-    #     # self.set_dark_mode(mode)
-    #     root.destroy()
-    #     self.__frame.config(bg=self.__colors['frame'])
-    #     self.convert_board_to_interface()
-    #     self.create_status_bars()
-
     def create_combo(self, frame, options):
         combo = ttk.Combobox(frame, value=options)
         combo.set('...')
@@ -597,11 +589,6 @@ class GUI:
         dark_mode_combo.bind('<<ComboboxSelected>>', lambda event: self.set_dark_mode(dark_mode_combo.get()))
         dark_mode_combo.grid(row=1, column=1, padx=10, pady=10)
 
-        # B1 = Button(frame, text="Okay", height=2, width=10, command=partial(self.save_changes, new_root),
-        #             bg=self.__colors['button leave'], fg=self.__colors['text'])
-        # B1.grid(row=2,  padx=10, pady=20, sticky = 'ns')
-        # B1.bind("<Enter>", lambda event, button=B1: self.on_enter(button))
-        # B1.bind("<Leave>", lambda event, button=B1: self.on_leave(button))
         new_root.mainloop()
 
 table = ChessTable()
